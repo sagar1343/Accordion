804075cdb63a9cfaad510af60f1c50df6cda5710
Sagar <sagarchakrawarti24@gmail.com>
2024-01-28 23:47:40 +0530

    one accordion is active at a time

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 const accordion = document.querySelector(".accordion");
+
 function changeIcon(element) {
   element.classList.toggle("rotateIcon");
 }
-accordion.addEventListener("click", function (event) {
-  if (event.target.tagName === "IMG") {
-    event.target.parentNode.nextElementSibling.classList.toggle(
-      "toggleDisplay"
-    );
-    changeIcon(event.target);
+
+function toggleAccordionContent(accordionItem) {
+  const accordionContent = accordionItem.querySelector(".accordion-content");
+  accordionContent.classList.toggle("active");
+}
+
+function handleClick(event) {
+  const accordionItem = event.target.closest(".accordion-item");
+
+  const isActive = accordionItem.querySelector(".accordion-content.active");
+
+  if (isActive) {
+    changeIcon(accordionItem.querySelector(".icon"));
+    toggleAccordionContent(accordionItem);
+  } else {
+    const activeItem = accordion.querySelector(".accordion-content.active");
+    if (activeItem) {
+      activeItem.classList.remove("active");
+      changeIcon(activeItem.closest(".accordion-item").querySelector(".icon"));
+    }
+    toggleAccordionContent(accordionItem);
+    changeIcon(accordionItem.querySelector(".icon"));
   }
-});
+}
+accordion.addEventListener("click", (e) => handleClick(e));
